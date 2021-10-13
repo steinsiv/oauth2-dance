b2a26cf563d6d3007d3036f0939ca2a73439f545
Stein A Sivertsen <steinsiv@users.noreply.github.com>
2021-10-13 20:28:56 +0200

    downgrade

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 import { config as dotEnvConfig } from "https://deno.land/x/dotenv@v3.0.0/mod.ts";
-import { Application, Context, Router } from "https://deno.land/x/oak@v9.0.1/mod.ts";
+import { Application, Context, Router } from "https://deno.land/x/oak@v9.0.0/mod.ts";
 import { createHash } from "https://deno.land/std@0.107.0/hash/mod.ts";
 import { cryptoRandomString } from "https://deno.land/x/crypto_random_string@1.1.0/mod.ts";
 import { DB as SqliteDB } from "https://deno.land/x/sqlite/mod.ts";
